a4a75f05cf77a3e5d33f10ee3ad1cb75610d4ee8
Khaibar Saadat <khybersaadat93@gmail.com>
2024-01-11 20:20:04 +0100

    How Not to Update the UI - A Look Behind the Scenes of React [Core Concept]

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,14 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  let tabContent = "Please click a button";
+
   function handleSelect(selectedButton) {
-    console.log(`${selectedButton} button was selected`);
+    tabContent = selectedButton;
+    console.log(tabContent);
   }
+
+  console.log("App is executed");
 
   return (
     <div>
@@ -36,7 +41,7 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          Dynamic Content
+          {tabContent}
         </section>
       </main>
     </div>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,4 +1,6 @@
 export default function TabButton({ children, onSelect }) {
+  console.log("TabButton is executed");
+
   return (
     <li>
       <button onClick={onSelect}>{children}</button>
